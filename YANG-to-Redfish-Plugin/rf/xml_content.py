05e8f2dd546cc5e3f9baa5d5151aaba643a206e1
Mike Raineri <michael.raineri@dell.com>
2023-03-17 14:36:51 -0400

    Updated license comments to point to new branch name
    Signed-off-by: Mike Raineri <michael.raineri@dell.com>

--- YANG-to-Redfish-Plugin/rf/xml_content.py
+++ YANG-to-Redfish-Plugin/rf/xml_content.py
@@ -1,6 +1,6 @@
 # Copyright Notice:
 # Copyright 2017-2019 DMTF. All rights reserved.
-# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/YANG-to-Redfish-Converter/blob/master/LICENSE.md
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/YANG-to-Redfish-Converter/blob/main/LICENSE.md
 
 grammar_whitespace_mode = 'optional'
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring, dump
--- src/postprocess.sh
+++ src/postprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright Notice:
 # Copyright 2017-2019 DMTF. All rights reserved.
-# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/YANG-to-Redfish-Converter/blob/master/LICENSE.md
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/YANG-to-Redfish-Converter/blob/main/LICENSE.md
 
 directory=$1
 for file in `ls $directory`; do
